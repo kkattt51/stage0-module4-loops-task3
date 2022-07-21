04d68d643689f734a6dfee494a4cbd5ec9b59f40
Kislitskaya_I <IKislitskaya@ibagroup.eu>
2022-07-21 17:55:49 +0300

    task3 module 4 loops stage0

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -1,7 +1,17 @@
 package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
-    public void printDigitsSum(int t){
-
+    public void printDigitsSum(int t) {
+        String num = t + "";
+        int sum = 0;
+        for (int i = 0; i < num.length(); i++) {
+            int digit = t % 10;
+            t /= 10;
+            sum += digit;
+        }
+        if (sum < 0) {
+            sum = -sum;
+        }
+        System.out.println(sum);
     }
 }
--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -2,6 +2,15 @@ package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
-
+        int sum = 0;
+        for (int i = 0; i < lengthOfLastNumber; i++) {
+            int num = 0;
+            for (int j = 1; j <= i; j++) {
+                num += 9 * Math.pow(10, j);
+            }
+            num += 9;
+            sum += num;
+        }
+        System.out.println(sum);
     }
 }
